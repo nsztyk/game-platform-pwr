f53883c59458904a93e3a9476f7f0aded25fd3c5
Norbert Sztyk <norbert2000@onet.eu>
2021-03-21 17:55:47 +0100

    [Server] Create some user validation & handle errors

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -4,28 +4,37 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 
 // Register user
-router.post('/register', async (req, res)  => {
+router.post('/register', async (req, res) => {
 
-  const {username, password: plainTextPassword} = req.body.user
+  const user = req.body.user
 
-  const password = await bcrypt.hash(plainTextPassword, 10); 
 
-  const user = new User({
-    username,
-    password
-  })
+  if (user == undefined || (!user.hasOwnProperty('username') || !user.hasOwnProperty('password')))
+    return res.json({ status: 'error', error: 'Invalid user parameters' })
 
-  user.save()
-    .then((result) => {
-      res.send(result)
+  const { username, password: plainTextPassword } = user;
+
+  // TODO Regex for password and username
+  const password = await bcrypt.hash(plainTextPassword, 10);
+
+  try {
+    const response = await User.create({
+      username,
+      password
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    console.log('User created succesfully', response);
+  } catch (error) {
+    if (error.code === 11000) {
+      // duplicate key
+      return res.json({ status: 'error', error: 'Username already exist' })
+    }
+    throw error
+  }
+  res.json({ status: 'ok' })
 })
 
 // Login user
-router.post('/login', (req,res) => {
+router.post('/login', (req, res) => {
   res.send(req.body)
 })
 
