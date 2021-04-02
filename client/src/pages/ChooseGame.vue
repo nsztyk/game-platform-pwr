542ddd9f7bd3fa0df31909aaece8bbb7aeac8883
Norbert Sztyk <norbert2000@onet.eu>
2021-04-02 19:15:21 +0200

    Working chat

--- client/src/pages/ChooseGame.vue
+++ client/src/pages/ChooseGame.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
+  <div class="flex flex-col w-1/4 mx-auto">
     <h1>You are log-in</h1>
     <button @click="logOut">
       log out
     </button>
+    <input type="text" placeholder="message" v-model="messageText" />
+    <button @click="sendMessage">Send</button>
+    <div>
+      <ul>
+        <li v-for="(message, index) in messages" :key="index">
+          {{ message }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -17,8 +26,10 @@ export default defineComponent({
   name: "ChooseGame",
   data() {
     return {
+      messageText: "",
       socket: io("http://localhost:3000"),
       users: [],
+      messages: [] as string[],
     };
   },
   methods: {
@@ -26,11 +37,31 @@ export default defineComponent({
       removeToken();
       router.push({ name: "Account" });
     },
+    sendMessage() {
+      this.socket.emit("send-chat-message", this.messageText);
+      this.messages.push(`You: ${this.messageText}`)
+      this.messageText = "";
+    },
   },
   created() {
-    this.socket.on("chat-message", (data) => {
-      console.log(data);
+    this.socket.emit(
+      "new-user",
+      Math.random()
+        .toString(36)
+        .slice(2)
+    );
+
+    this.socket.on("chat-message", ({ message, nickname }) => {
+      this.messages.push(`${nickname}: ${message}`);
     });
+
+    this.socket.on("user-connected", nickname => {
+      this.messages.push(`${nickname} joined!`);
+    });
+
+    this.socket.on("user-disconnected", nickname => {
+      this.messages.push(`${nickname} disconneted!`)
+    })
   },
 });
 </script>
--- server/IoServ.js
+++ server/IoServ.js
@@ -4,6 +4,23 @@ const io = require("socket.io")(3000, {
   },
 });
 
+const users = {}
+
 io.on('connection', socket => {
-  socket.emit('chat-message', 'Hello World')
+
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', {message: message, nickname: users[socket.id]})
+  })
+
+  socket.on('disconnect', () => {
+    console.log("XD");
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
 })
+
