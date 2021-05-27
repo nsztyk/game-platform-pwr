6329f7e72c2b7a3735d582d6f8cf12c70311e58f
Norbert Sztyk <norbert2000@onet.eu>
2021-05-27 10:44:41 +0200

    Update chat view

--- client/src/components/RoomChat.vue
+++ client/src/components/RoomChat.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="custom-shadow bg-gray-900 col-span-2">
-    <input type="text" placeholder="message" v-model="messageText" />
-    <button @click="sendMessageToOthers">Send</button>
-    <div>
+  <div class="custom-shadow bg-gray-900 col-span-2 p-3">
+    <div class="wrapper rounded-lg px-4 py-2 flex justify-between mb-4">
+      <input
+        type="text"
+        placeholder="Message"
+        v-model="messageText"
+        class="bg-transparent border-b-2 border-gray-200 outline-none"
+      />
+      <label
+        type="button"
+        @click="sendMessageToOthers"
+        class="border-2 border-gray-200 rounded-lg px-4 outline-none cursor-pointer"
+        :class="{'disabled' : isDisabled}"
+        >Send</label
+      >
+    </div>
+    <div class="wrapper rounded-lg px-4 py-2" v-if="getMessages.length">
       <ul>
-        <li v-for="(message, index) in getMessages" :key="index">
+        <li v-for="(message, index) in getMessages" :key="index" class="leading-relaxed">
           {{ message }}
         </li>
       </ul>
@@ -13,24 +26,36 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
 import { getMessages, sendMessage } from "../middleware/SocketConnection";
 export default defineComponent({
   setup() {
+    const messageText = ref("");
+
+    const sendMessageToOthers = () => {
+      if (messageText.value.length) sendMessage(messageText.value);
+      messageText.value = "";
+    };
+
+    const isDisabled = computed(() => !messageText.value.length);
+
     return {
       getMessages,
+      sendMessageToOthers,
+      isDisabled,
+      messageText,
     };
-  },
-  data(){
-    return {
-      messageText: ""
-    }
-  },
-  methods: {
-    sendMessageToOthers() {
-      sendMessage(this.messageText);
-      this.messageText = "";
-    },
   },
 });
 </script>
+
+<style scoped>
+.wrapper {
+  background-color: #0b111e;
+}
+
+.disabled {
+  opacity: 0.6;
+  cursor: default;
+}
+</style>
